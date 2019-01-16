91e0c1747e11aa3e582b9c2a6c0e2982d4a598ff
“Podrazka” <rafal.podrazka@stepstone.com>
2019-01-16 18:06:15 +0100

    remove unused imports

--- src/main/java/com/stepstone/arena/CreateCreatureController.java
+++ src/main/java/com/stepstone/arena/CreateCreatureController.java
@@ -1,9 +1,6 @@
 package com.stepstone.arena;
 
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
